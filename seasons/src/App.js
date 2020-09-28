8c39d568fbbf89b5390b9c21a73f6b74f17dac14
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-09-28 23:59:42 +0300

    created app 'seasons', create state and request location

---
+++ seasons/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+import SeasonDisplay from './components/SeasonDisplay';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //STATE
+    this.state = {
+      latitude: null,
+      errorMessage: '',
+    };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //change STATE
+        this.setState({
+          ...this.state,
+          latitude: position.coords.latitude,
+        });
+      },
+      (error) => {
+        this.setState({
+          ...this.state,
+          errorMessage: error.message,
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.errorMessage && !this.state.latitude && (
+          <div>Error: {this.state.errorMessage}</div>
+        )}
+        {!this.state.errorMessage && this.state.latitude && (
+          <div>Latitude: {this.state.latitude}</div>
+        )}
+        {!this.state.errorMessage && !this.state.latitude && (
+          <div>Loading...</div>
+        )}
+        {/* <SeasonDisplay /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
